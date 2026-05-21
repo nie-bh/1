4e6121b8a9177b96d48fd5e71455b59487c44605
nie-bh <tiwarishashank452@gmail.com>
2026-05-21 23:13:48 +0530

    Delete datetime_app directory

--- datetime_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatetimeAppConfig(AppConfig):
-    name = 'datetime_app'
--- datetime_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Route the root URL to our dynamic calculator view
-    path('', views.time_calculator_view, name='time_calculator_view'),
-]
